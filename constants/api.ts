bb16b8ac505bb114564805a651912e0ac6dbd177
Jacques Beeets <jacques@hellofirstview.com>
2022-08-09 11:28:40 +0200

    added constants

---
+++ constants/api.ts
@@ -0,0 +1,22 @@
+export default {
+    types:{
+        COLLECTION: "collection",
+        MOVIE: "movie",
+        SERIES: "series",
+        EPISODES: "episode",
+    },
+    genres:{
+        ACTION: "action",
+        ADVENTURE: "adventure",
+        ANIMATION: "animation"
+    },
+    OBJECT_KEY_PREFIX: "https://d1chb2avsvwxer.cloudfront.net/",
+    TYPES_URL: "https://sqjqk3kqk3.execute-api.eu-west-1.amazonaws.com/stage/content/type/",
+    ITEM_GUID_URL: "https://sqjqk3kqk3.execute-api.eu-west-1.amazonaws.com/stage/content/",
+    RETURN_GENRE_URL(genre){
+        if(genre){
+            return `https://sqjqk3kqk3.execute-api.eu-west-1.amazonaws.com/stage/content/genre?genres[]=${genre}&genres[]=${genre}`
+        }
+        return false 
+    }
+}
